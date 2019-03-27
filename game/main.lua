1de70080bfd2813381a4cd0a987c1f801d6237de
Lucas Eduardo <33419659+MaiaLucas@users.noreply.github.com>
2019-03-27 17:31:36 -0300

    Update main.lua

--- game/main.lua
+++ game/main.lua
@@ -7,6 +7,8 @@ physics.start()
  
 local scene = composer.newScene()
 
+math.randomseed( os.time() )
+
 local meters = 0
 local metersText
 local metersLoop
@@ -15,21 +17,25 @@ local score = 0
 local scoreText
 local scoreLoop
 
-local scenes
+local cloundScene
+local grassScene
+local backgroundScene
 
 local level = 1
 local velocityLevel = level*0.5
 local updateLevel
 
+local life = 4
+local cont = 0
+
 local extraPointTable = {}
 local obstacleTable = {}
 local scenesTable = {}
 
-math.randomseed( os.time() )
--- Show Background Level 1
-local background = display.newImageRect("images/background-day.jpg", 600, 380)
-background.x = display.contentCenterX
-background.y = display.contentCenterY
+-- Background day
+local background1 = display.newImageRect("images/background/amanhecer.png", 600, 380)
+background1.x = display.contentCenterX
+background1.y = display.contentCenterY
 
 -- Meters bar
 local uiGroup = display.newGroup()
@@ -55,26 +61,89 @@ sky.name = "Sky"
 physics.addBody( sky, "static" )
 
 -- Show the square
-local square = display.newImageRect("images/quadrado.png", 30, 30)
+local square = display.newImageRect("images/square/full-life.png", 30, 30)
 square.x = display.contentCenterX-200
 square.y = display.contentCenterY+100
 square.myName = "square"
-physics.addBody( square, "dynamic", { bounce=-0.1 } )
+physics.addBody( square, "dynamic", { bounce= 0 } )
 
 --------------- Functions ---------------
-
 local function pushSquare()
     square:applyLinearImpulse( 0, -0.05, square.x, square.y )
 end
+background1:addEventListener( "tap", pushSquare )
 
-background:addEventListener( "tap", pushSquare )
+---- Background movimentation 
+local function cloudMoviment()
+
+    local nuvem = display.newImageRect("images/nuvem.png", 95, 55 )
+    physics.addBody( nuvem, "dynamic", { bounce = 0, isSensor = true } )
+    nuvem.gravityScale = 0
+    nuvem.myName = "nuvem"
+    nuvem.x = display.contentWidth+100
+    nuvem.y = display.contentHeight-math.random(150, 250)
+    nuvem.alpha = 0.5
+    nuvem:setLinearVelocity( -100, 0 )
+
+    scenesTable[#scenesTable+1] = nuvem
+
+end
+
+local function grassMoviment()
+
+    local grass = display.newImageRect("images/grass-test.png", 75, 15 )
+    physics.addBody( grass, "dynamic", { bounce = 0, isSensor = true } )
+    grass.gravityScale = 0
+    grass.myName = "nuvem"
+    grass.x = display.contentWidth+100
+    grass.y = 282
+    grass.alpha = 0.3
+    grass:setLinearVelocity( -100, 0 )
+
+    scenesTable[#scenesTable+1] = grass
+
+end
+
+local function changeBackground()
+
+    background1.alpha = 0
+    cont = cont + 1
+    if( cont == 1 ) then
+        local background = display.newImageRect("images/background/amanhecer.png", 600, 380)
+        background.x = display.contentCenterX
+        background.y = display.contentCenterY
+
+    elseif( cont == 2 ) then
+        local background = display.newImageRect("images/background/manha.png", 600, 380)
+        background.x = display.contentCenterX
+        background.y = display.contentCenterY
+
+    elseif( cont == 3 ) then
+        local background = display.newImageRect("images/background/meio-dia.png", 600, 380)
+        background.x = display.contentCenterX
+        background.y = display.contentCenterY
+
+    elseif( cont == 4 ) then
+        local background = display.newImageRect("images/background/entardecer.png", 600, 380)
+        background.x = display.contentCenterX
+        background.y = display.contentCenterY
+
+    elseif( cont == 5 ) then
+        local background = display.newImageRect("images/background/noite.png", 600, 380)
+        background.x = display.contentCenterX
+        background.y = display.contentCenterY
+
+        cont = 0
+    end
+
+end
+
+--backgroundScene = timer.performWithDelay( 500, changeBackground, -1 )
 
 local function updateVelocity()
     level = level + 1 
+    velocityLevel = level*0.5
 end
-velocityLevel = level*0.5
-
-updateLevel = timer.performWithDelay( 100000, updateVelocity, -1 )
 
 ---- Create obstacles
 local function createObstacles()
@@ -125,75 +194,41 @@ end
 local function extraPoint()
 
     local whereFrom = math.random( 2 )
+    whereFrom = 1
     local extra
 
     if ( whereFrom == 1 ) then
-        -- Triangle
-        extra = display.newImageRect("images/pontos-extras-1.png", 40, 40 )
-        physics.addBody( extra, "dynamic", { bounce = 0, isSensor = true } )
-        extra.gravityScale = 10
-        extra.myName = "extra"
-        extra.x = display.contentWidth+100
-        extra.y = display.contentHeight-math.random( 50, 200 )
-        extra:setLinearVelocity( -100*velocityLevel, 0 )
-
-        extraPointTable[#extraPointTable+1] = extra
-
-    elseif( whereFrom == 2 ) then
-        -- Circle
-        extra = display.newImageRect( "images/pontos-extras-2.png", 40, 40 )
+        -- square
+        extra = display.newImageRect("images/square/1-life.png", 40, 40 )
         physics.addBody( extra, "dynamic", { bounce = 0, isSensor = true } )
         extra.gravityScale = 0
         extra.myName = "extra"
         extra.x = display.contentWidth+100
-        extra.y = display.contentHeight-math.random( 100, 200 )
-        extra:setLinearVelocity( -100*velocityLevel, -10 )
+        extra.y = display.contentHeight-math.random( 50, 200 )
+        extra:setLinearVelocity( -80*velocityLevel, 0 )
 
         extraPointTable[#extraPointTable+1] = extra
     end
+    -- elseif( whereFrom == 2 ) then
+    --     -- Circle
+    --     extra = display.newImageRect( "images/pontos-extras-2.png", 40, 40 )
+    --     physics.addBody( extra, "dynamic", { bounce = 0, isSensor = true } )
+    --     extra.gravityScale = 0
+    --     extra.myName = "extra"
+    --     extra.x = display.contentWidth+100
+    --     extra.y = display.contentHeight-math.random( 100, 200 )
+    --     extra:setLinearVelocity( -100*velocityLevel, -10 )
+
+    --     extraPointTable[#extraPointTable+1] = extra
+    -- end
 
 end
-
----- Background movimentation 
-local function cloudMoviment()
-
-    local nuvem = display.newImageRect("images/nuvem.png", 95, 55 )
-    physics.addBody( nuvem, "dynamic", { bounce = 0, isSensor = true } )
-    nuvem.gravityScale = 0
-    nuvem.myName = "nuvem"
-    nuvem.x = display.contentWidth+100
-    nuvem.y = display.contentHeight-math.random(150, 200)
-    nuvem.alpha = 0.5
-    nuvem:setLinearVelocity( -100, 0 )
-
-    scenesTable[#scenesTable+1] = nuvem
-
-end
-
-local function grassMoviment()
-
-    local grass = display.newImageRect("images/grass.png", 15, 55 )
-    physics.addBody( grass, "dynamic", { bounce = 0, isSensor = true } )
-    grass.gravityScale = 0
-    grass.myName = "nuvem"
-    grass.x = display.contentWidth+100
-    grass.y = 288
-    grass.alpha = 0.5
-    grass:setLinearVelocity( -100, 0 )
-
-    scenesTable[#scenesTable+1] = grass
-
-end
-scenes = timer.performWithDelay( 150, grassMoviment, -1 )
-scenes = timer.performWithDelay( 3000, cloudMoviment, -1 )
 
 ---- Game looping
 local function updateDistance()
     meters = meters + 1
     metersText.text = meters .. " m"  
 end
-
-metersLoop = timer.performWithDelay( 500/velocityLevel, updateDistance, -1 )
 
 local function gameLoop()
 
@@ -239,8 +274,27 @@ local function gameLoop()
     return true
 end
 
-scoreLoop = timer.performWithDelay( 9000*velocityLevel, extraPoint, 0 )
+--- Looping
+-- Scenes
+cloundScene = timer.performWithDelay( 500, grassMoviment, 0 )
+grassScene = timer.performWithDelay( 3000, cloudMoviment, 0 )
+-- Game
+metersLoop = timer.performWithDelay( 500/velocityLevel, updateDistance, -1 )
+scoreLoop = timer.performWithDelay( 50000*velocityLevel, extraPoint, 0 )
+updateLevel = timer.performWithDelay( 50000, updateVelocity, -1 )
 gameLoopTimer = timer.performWithDelay( 5000-(500/velocityLevel), gameLoop, 0 )
+
+local function showLife()
+
+    if( life == 3 ) then
+        square = display.newImageRect("images/3-life.png", 30, 30)
+        square.x = display.contentCenterX-200
+        square.y = display.contentCenterY+100
+        square.myName = "square"
+        physics.addBody( square, "dynamic", { bounce= 0 } )
+    end
+
+end
 
 local function onCollision( event )
  
@@ -265,25 +319,29 @@ local function onCollision( event )
         scoreText.text = score
 
     end
-        
+
     if ( ( obj1.myName == "square" and obj2.myName == "obstacle" ) or 
          ( obj1.myName == "obstacle" and obj2.myName == "square" ) ) then
 
+        life = life - 1
+
+        display.remove(obj1)
+        display.remove(obj2)
+                    
         --
         timer.cancel(metersLoop)
         timer.cancel(scoreLoop)
         timer.cancel(updateLevel)
-        timer.cancel(scenes)
-
-        display.remove( obj1 )
-        display.remove( obj2 )
-
+        timer.cancel(cloundScene)
+        timer.cancel(grassScene)
         timer.cancel(gameLoopTimer)
+        --
 
         local text = display.newText("Distância: " .. meters .. " m", display.contentCenterX, 150, native.systemFont, 25)
         local text2 = display.newText("Scores: " .. score, display.contentCenterX, 100, native.systemFont, 30)
         text2:setFillColor( 0.2, 0.6, 0.9 )
         text:setFillColor( 0 )
+
     end
     --
 end
