6e1f38058231b9eb6a7169bc88e165e095c49651
Lucas Eduardo <33419659+MaiaLucas@users.noreply.github.com>
2019-05-04 20:49:19 -0300

    Adiconando novos canais de musica

--- main.lua
+++ main.lua
@@ -6,8 +6,11 @@ audio.reserveChannels( 1 ) -- musica menu
 audio.reserveChannels( 2 ) -- musica de game over
 audio.reserveChannels( 3 ) -- musica do jogo
 audio.reserveChannels( 4 ) -- musica de pulo
+audio.reserveChannels( 5 ) -- musica de perca de vida
+audio.reserveChannels( 6 ) -- musica de ganho de vida
 
 menumusic = audio.loadSound( "sound/menu.wav" )
 audio.play( menumusic, {channel=1, loops=0} )
+audio.setMaxVolume( 0.40, {channel=1} )
 
 composer.gotoScene( "menu" )
